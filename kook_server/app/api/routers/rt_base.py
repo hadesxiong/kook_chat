663684eaf3632b1fa1ed7b9db5b0255ae6a9bf78
hadesxiong <fengshixiong890614@gmail.com>
2024-12-10 16:09:02 +0800

    update zlib related function

--- kook_server/app/api/routers/rt_base.py
+++ kook_server/app/api/routers/rt_base.py
@@ -1,7 +1,7 @@
 # coding=utf8
 import zlib,json,re
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 from app.utils.decrypt import CookEncrypt
 from app.core.config import settings
@@ -12,31 +12,31 @@ base_rt = APIRouter(prefix='/base', tags=['base'])
 # 验证webhook服务器
 @base_rt.post('/challenge')
 
-async def handleChallenge(form_data:dict):
+async def handleChallenge(request:Request):
 
-    print(form_data)
-
-    # decompress_data = zlib.decompress(form_data)
-    # print(decompress_data)
-
-    decompress_data = form_data
+    body = await request.body()
+    decompress_data = zlib.decompress(body)
 
     kook_encryptor = CookEncrypt(settings.KOOK_KEY)
-    decrypt_str = kook_encryptor.aes_decrypt(form_data.get('encrypt'))
-    re_match = re.search(r'\{[^{}]*(?:\{[^{}]*\}[^{}]*)*\}', decrypt_str)
+    decrypt_str = kook_encryptor.aes_decrypt(decompress_data.get('encrypt'))
+    # re_match = re.search(r'\{[^{}]*(?:\{[^{}]*\}[^{}]*)*\}', decrypt_str)
     
-    if re_match:
-        decrypt_str = re_match.group(0)
-        print(decrypt_str)
-        decrypt_data = json.loads(decrypt_str)
+    # if re_match:
+    #     decrypt_str = re_match.group(0)
+    #     print(decrypt_str)
+    #     decrypt_data = json.loads(decrypt_str)
         
-        try:
-            challenge = decrypt_data.get('d').get('challenge')
-            return {'challenge': challenge}
+    #     try:
+    #         challenge = decrypt_data.get('d').get('challenge')
+    #         return {'challenge': challenge}
         
-        except:
-            print('fail')
-            return {'challenge': 'fail'}
+    #     except:
+    #         print('fail')
+    #         return {'challenge': 'fail'}
 
-    else:
-        return 1
+    # else:
+    #     return 1
+    
+    decrypt_data = json.loads(decrypt_str)
+    challenge = decrypt_data.get('d').get('challenge')
+    return {'challenge': challenge}
